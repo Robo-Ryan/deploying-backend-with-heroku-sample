9ac9c9a47e65f24f95b03f8a51668a8467fd7386
Robo-Ryan <rdmerlini@gmail.com>
2024-08-22 11:17:31 -0400

    Update app.js to cat facts
    updated boredapi to cat facts api

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const app = express(); // create an instance of the express module (app is the c
 const fetch = require('node-fetch'); // import node-fetch (enables the fetch API to be used server-side)
 const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (5000) on your machine
 
-app.get('/', (req, res) => { // send a get request to root directory ('/' is this file (app.js))
-  fetch('https://www.boredapi.com/api/activity') // fetch activity from bored API - https://www.boredapi.com/about
-    .then(res => res.json()) // return a promise containing the response
-    .then(json => res.send(`<h1>Today's Activity: ${json.activity}!</h1>`)) // extract the JSON body content from the response (specifically the activity value) and sends it to the client
-    .catch(function(err){ // catch any errors
-      console.log(err); // log errors to the console
-    })
-})
+app.get('/', (req, res) => {
+  // Fetch a random cat fact from the Cat Facts API
+  fetch('https://catfact.ninja/fact')
+    .then(response => response.json()) // Return a promise containing the response in JSON format
+    .then(json => res.send(`<h1>Did you know? ${json.fact}</h1>`)) // Extract the 'fact' value from the JSON and send it to the client
+    .catch(function(err){ // Catch any errors
+      console.log(err); // Log errors to the console
+      res.status(500).send('Something went wrong, please try again later.');
+    });
+});
+
 
 app.listen(PORT, () => { // start server and listen on specified port
   console.log(`App is running on ${PORT}`) // confirm server is running and log port to the console
